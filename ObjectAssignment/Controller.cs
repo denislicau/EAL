f5f7792faf001d31f8cbf098f3e14138e5cade42
Elvinas <elvinasjarusevicius@gmail.com>
2014-11-11 10:10:49 +0100

    Revert "Fourth Commit"
    This reverts commit a3b3b96896c1f232f8f28116a71528f03b2541e0.

--- ObjectAssignment/Controller.cs
+++ ObjectAssignment/Controller.cs
@@ -21,9 +21,6 @@ namespace ObjectAssignment
             // a long description
             // a deadline
             // and one or more tasks that each contains a description
-
-
-            //////////////////////////EVEN MORE CHANGES
         }
 
         /// <summary>
